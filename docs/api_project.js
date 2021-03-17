c213fc7c12072b0dbc4ce03e0df5ee63e5b1976c
matheusrmeloo <matheusrodrigues030717@gmail.com>
2021-03-17 00:34:11 -0300

    docs: add all documentation

---
+++ docs/api_project.js
@@ -0,0 +1,16 @@
+define({
+  "name": "API - BOVCONTROL",
+  "version": "0.0.1",
+  "description": "Doc API Basic",
+  "title": "BovControlAPI",
+  "url": "https://bovcontrol-dev.herokuapp.com/api",
+  "sampleUrl": false,
+  "defaultVersion": "0.0.0",
+  "apidoc": "0.3.0",
+  "generator": {
+    "name": "apidoc",
+    "time": "2021-03-17T03:33:52.473Z",
+    "url": "https://apidocjs.com",
+    "version": "0.26.0"
+  }
+});
